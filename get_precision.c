f5fd5fd8c7bec26da65e8bc676caa206c76e2c6b
lloyd72 <lloydsintim128@gmail.com>
2023-03-28 14:06:12 -0700

    C - printf

--- get_precision.c
+++ get_precision.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision printing it.
- * @format: Formatted string  to print the arguments.
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
@@ -20,22 +20,23 @@ int get_precision(const char *format, int *i, va_list list)
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-	if (is_digit(format[curr_i]))
-	{
-		precision *= 10;
-		precision += format[curr_i] - '0';
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
 		}
-	else if (format[curr_i] == '*')
-	{
-		curr_i++;
-	precision = va_arg(list, int);
-	break;
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
 		}
-	else
-		break;
+		else
+			break;
 	}
 
 	*i = curr_i - 1;
 
 	return (precision);
 }
+
